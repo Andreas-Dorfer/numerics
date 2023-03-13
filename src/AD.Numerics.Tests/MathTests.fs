20f6cbdafedd680ec2b195b3930ee915c0f5505d
Andreas Dorfer <41114919+Andreas-Dorfer@users.noreply.github.com>
2023-03-13 18:53:01 +0100

    ref

--- src/AD.Numerics.Tests/MathTests.fs
+++ src/AD.Numerics.Tests/MathTests.fs
@@ -2,7 +2,7 @@ namespace AD.Numerics.Tests
 
 open Microsoft.VisualStudio.TestTools.UnitTesting
 open AD.FsCheck.MSTest
-open System
+open type System.Math
 open AD.Numerics
 
 [<TestClass>]
@@ -13,7 +13,7 @@ type MathTests () =
         actual =! expected
 
     [<Property>]
-    member _.RoundDecimal (mode, (Digits digits), a: decimal) = Math.Round(a, digits, mode) |> assertRound mode digits a
-
+    member _.RoundFloat (mode, (Digits digits), a: float) = Round(a, digits, mode) |> assertRound mode digits a
+    
     [<Property>]
-    member _.RoundFloat (mode, (Digits digits), a: float) = Math.Round(a, digits, mode) |> assertRound mode digits a
+    member _.RoundDecimal (mode, (Digits digits), a: decimal) = Round(a, digits, mode) |> assertRound mode digits a
